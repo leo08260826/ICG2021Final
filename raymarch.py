6b26b7b62afd79dd57004a84807a6f8329833af3
Chin-Tang Chen <chintang@cml18>
2021-06-20 08:44:50 +0800

    polygon as minimal shape

--- raymarch.py
+++ raymarch.py
@@ -19,51 +19,42 @@ def DE_inf_ball(point):
 	return np.linalg.norm(point)-0.3
 
 @jit(nopython=True, nogil=True)
-def DE_tetrahedron(point):
-	point = rotation_matrix(0.48, 0.7, 0.29) @ point
+def DE_tetrahedron(p):
+	p[2] += 6
+	p = rotation_matrix(0.48, 0.7, 0.29) @ p
 	iteration = 10
 	scale = 2.0
 	reflect_normals = np.array([[1,1,0],[1,0,1],[0,1,1]]) / np.sqrt(2)
 	offset = np.array([1,1,1])
 	for _ in range(iteration):
-		point -= 2.0 * min(0.0, np.dot(point, reflect_normals[0])) * reflect_normals[0]
-		point -= 2.0 * min(0.0, np.dot(point, reflect_normals[1])) * reflect_normals[1]
-		point -= 2.0 * min(0.0, np.dot(point, reflect_normals[2])) * reflect_normals[2]
-		point = point * scale - offset * (scale - 1.0)
-	return np.linalg.norm(point) * pow(scale, float(-iteration))
+		p -= 2.0 * min(0.0, np.dot(p, reflect_normals[0])) * reflect_normals[0]
+		p -= 2.0 * min(0.0, np.dot(p, reflect_normals[1])) * reflect_normals[1]
+		p -= 2.0 * min(0.0, np.dot(p, reflect_normals[2])) * reflect_normals[2]
+		p = p * scale - offset * scale
+	p /= scale
+	tet_norm = np.array([[-1,1,1],[1,-1,1],[1,1,-1],[-1,-1,-1]]) / np.sqrt(3)
+	tet_offs = np.array([[1,1,1],[1,1,1],[1,1,1],[1,-1,-1]])
+	tet_dist = ((p - tet_offs) * tet_norm).sum(axis=1).max()
+	return tet_dist * pow(scale, float(-iteration+1))
 
 @jit(nopython=True, nogil=True)
 def DE_cube(p):
+	p[2] += 3
+	#p = rotation_matrix(0.6*p[0], 0, 0) @ p
 	p = rotation_matrix(0.79, 0.7, 0.63) @ p
-# 	iteration = 10
-# 	scale = 3.0
-# 	nor = np.array([[1.0,0,0],[0,1.0,0],[0,0,1.0],[1.0,0,0],[0,1.0,0],[0,0,1.0]])
-# 	off = np.array([[1/3,0,0],[0,1/3,0],[0,0,1/3],[0.0,0,0],[0,0.0,0],[0,0,0.0]])
-# 	init = np.array([1,1,1])
-# 	for _ in range(iteration):
-# 		p -= 2.0 * min(0.0, np.dot(p - off[3], nor[3])) * nor[3] + off[3]
-# 		p -= 2.0 * min(0.0, np.dot(p - off[4], nor[4])) * nor[4] + off[4]
-# 		p -= 2.0 * min(0.0, np.dot(p - off[5], nor[5])) * nor[5] + off[5]
-# 		dot0 = np.dot(p - off[0], nor[0])
-# 		dot1 = np.dot(p - off[1], nor[1])
-# 		dot2 = np.dot(p - off[2], nor[2])
-# 		if dot0 < 0 and dot1 >= 0 and dot2 >= 0:
-# 			p -= 2.0 * dot0 * nor[0]
-# 		if dot1 < 0 and dot0 >= 0 and dot2 >= 0:
-# 			p -= 2.0 * dot1 * nor[1]
-# 		if dot2 < 0 and dot1 >= 0 and dot0 >= 0:
-# 			p -= 2.0 * dot2 * nor[2]
-# 		p = p * scale - init * (scale - 1.0)
-# 	return np.linalg.norm(p) * pow(scale, float(-iteration))
 	iteration = 10
 	scale = 3.0
 	vtx = np.array([[-1,-1,-1],[-1,-1,0],[-1,-1,1],[-1,0,-1],[-1,0,1],[-1,1,-1],[-1,1,0],[-1,1,1],\
                     [0,-1,-1],[0,-1,1],[0,1,-1],[0,1,1],\
-                    [1,-1,-1],[1,-1,0],[1,-1,1],[1,0,-1],[1,0,1],[1,1,-1],[1,1,0],[1,1,1]])/1.5
+                    [1,-1,-1],[1,-1,0],[1,-1,1],[1,0,-1],[1,0,1],[1,1,-1],[1,1,0],[1,1,1]])
 	for _ in range(iteration):
 		v = np.square(vtx - p).sum(axis=1).argmin()
-		p = p * scale - vtx[v] * (scale - 0.0)
-	return np.linalg.norm(p) * pow(scale, float(-iteration))
+		p = p * scale - vtx[v] * scale
+	p /= scale
+	cube_norm = np.array([[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]])
+	cube_offs = np.array([[1,1,1],[-1,-1,-1],[1,1,1],[-1,-1,-1],[1,1,1],[-1,-1,-1]])
+	cube_dist = ((p - cube_offs) * cube_norm).sum(axis=1).max()
+	return cube_dist * pow(scale, float(-iteration+1))
 
 # @jit(nopython=True, nogil=True)
 # def hsv_to_rgb(h, s, v):
